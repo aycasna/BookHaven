be87a77f121000613b3651a9c1d5971a88010a90
ayca <aycasena168@gmail.com>
2024-05-07 20:12:51 +0300

    Add project files.

---
+++ BookHaven/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace BookHaven.Models
+{
+    public class Book
+    {
+        public int bookID { get; set; }
+        public string title { get; set; }
+        public string authors { get; set; }
+        public float average_rating { get; set; }
+        public string isbn { get; set; }
+        public int isbn13 { get; set; }
+        public string language_code { get; set; }
+        public int num_pages { get; set; }
+        public int ratings_count { get; set; }
+        public int text_reviews_count { get; set; }
+        public DateOnly publication_date { get; set; }
+        public string publisher { get; set; }
+
+    }
+}
---
+++ BookHaven/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookHaven.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int Rating { get; set; }
+        public string Content { get; set; }
+
+        [Required] 
+        public string IdentityUserId { get; set; }
+        [ForeignKey("IdentityUserId")]
+        public IdentityUser User { get; set; }
+
+        public int bookID { get; set; }
+        [ForeignKey("bookID")]
+    }
+}
